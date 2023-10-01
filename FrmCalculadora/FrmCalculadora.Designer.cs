44ca820e938b0da864b7f30b38bac8576fd1bb18
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2023-09-30 21:54:51 -0300

    Form Completado

--- FrmCalculadora/FrmCalculadora.Designer.cs
+++ FrmCalculadora/FrmCalculadora.Designer.cs
@@ -28,18 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCalculadora));
+            txtPrimerOperador = new TextBox();
+            txtSegundoOperador = new TextBox();
+            cmbOperacion = new ComboBox();
+            btnOperar = new Button();
+            btnLimpiar = new Button();
+            btnCerrar = new Button();
+            lblResultado = new Label();
+            lblPrimerOperador = new Label();
+            lblOperacion = new Label();
+            lblSegundoOperador = new Label();
+            grpSistema = new GroupBox();
+            rdbDecimal = new RadioButton();
+            rdbBinario = new RadioButton();
+            grpSistema.SuspendLayout();
             SuspendLayout();
+            // 
+            // txtPrimerOperador
+            // 
+            txtPrimerOperador.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPrimerOperador.Location = new Point(12, 297);
+            txtPrimerOperador.Name = "txtPrimerOperador";
+            txtPrimerOperador.Size = new Size(240, 52);
+            txtPrimerOperador.TabIndex = 0;
+            // 
+            // txtSegundoOperador
+            // 
+            txtSegundoOperador.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSegundoOperador.Location = new Point(548, 297);
+            txtSegundoOperador.Name = "txtSegundoOperador";
+            txtSegundoOperador.Size = new Size(240, 52);
+            txtSegundoOperador.TabIndex = 1;
+            // 
+            // cmbOperacion
+            // 
+            cmbOperacion.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOperacion.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            cmbOperacion.FormattingEnabled = true;
+            cmbOperacion.Location = new Point(336, 296);
+            cmbOperacion.Name = "cmbOperacion";
+            cmbOperacion.Size = new Size(121, 53);
+            cmbOperacion.TabIndex = 2;
+            // 
+            // btnOperar
+            // 
+            btnOperar.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOperar.Location = new Point(12, 374);
+            btnOperar.Name = "btnOperar";
+            btnOperar.Size = new Size(240, 50);
+            btnOperar.TabIndex = 3;
+            btnOperar.Text = "Operar";
+            btnOperar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLimpiar.Location = new Point(276, 374);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(240, 50);
+            btnLimpiar.TabIndex = 4;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCerrar.Location = new Point(548, 374);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(240, 50);
+            btnCerrar.TabIndex = 5;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Font = new Font("Segoe UI", 35F, FontStyle.Regular, GraphicsUnit.Point);
+            lblResultado.Location = new Point(12, 29);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(243, 62);
+            lblResultado.TabIndex = 6;
+            lblResultado.Text = "Resultado:";
+            // 
+            // lblPrimerOperador
+            // 
+            lblPrimerOperador.AutoSize = true;
+            lblPrimerOperador.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPrimerOperador.Location = new Point(21, 234);
+            lblPrimerOperador.Name = "lblPrimerOperador";
+            lblPrimerOperador.Size = new Size(221, 37);
+            lblPrimerOperador.TabIndex = 7;
+            lblPrimerOperador.Text = "Primer Operador:";
+            // 
+            // lblOperacion
+            // 
+            lblOperacion.AutoSize = true;
+            lblOperacion.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            lblOperacion.Location = new Point(325, 234);
+            lblOperacion.Name = "lblOperacion";
+            lblOperacion.Size = new Size(146, 37);
+            lblOperacion.TabIndex = 8;
+            lblOperacion.Text = "Operacion:";
+            // 
+            // lblSegundoOperador
+            // 
+            lblSegundoOperador.AutoSize = true;
+            lblSegundoOperador.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSegundoOperador.Location = new Point(548, 234);
+            lblSegundoOperador.Name = "lblSegundoOperador";
+            lblSegundoOperador.Size = new Size(250, 37);
+            lblSegundoOperador.TabIndex = 9;
+            lblSegundoOperador.Text = "Segundo Operador:";
+            // 
+            // grpSistema
+            // 
+            grpSistema.Controls.Add(rdbBinario);
+            grpSistema.Controls.Add(rdbDecimal);
+            grpSistema.Location = new Point(300, 142);
+            grpSistema.Name = "grpSistema";
+            grpSistema.Size = new Size(200, 80);
+            grpSistema.TabIndex = 10;
+            grpSistema.TabStop = false;
+            grpSistema.Text = "Representar resultado en:";
+            // 
+            // rdbDecimal
+            // 
+            rdbDecimal.AutoSize = true;
+            rdbDecimal.Location = new Point(6, 37);
+            rdbDecimal.Name = "rdbDecimal";
+            rdbDecimal.Size = new Size(68, 19);
+            rdbDecimal.TabIndex = 0;
+            rdbDecimal.TabStop = true;
+            rdbDecimal.Text = "Decimal";
+            rdbDecimal.UseVisualStyleBackColor = true;
+            // 
+            // rdbBinario
+            // 
+            rdbBinario.AutoSize = true;
+            rdbBinario.Location = new Point(123, 37);
+            rdbBinario.Name = "rdbBinario";
+            rdbBinario.Size = new Size(62, 19);
+            rdbBinario.TabIndex = 1;
+            rdbBinario.TabStop = true;
+            rdbBinario.Text = "Binario";
+            rdbBinario.UseVisualStyleBackColor = true;
             // 
             // FrmCalculadora
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(grpSistema);
+            Controls.Add(lblSegundoOperador);
+            Controls.Add(lblOperacion);
+            Controls.Add(lblPrimerOperador);
+            Controls.Add(lblResultado);
+            Controls.Add(btnCerrar);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnOperar);
+            Controls.Add(cmbOperacion);
+            Controls.Add(txtSegundoOperador);
+            Controls.Add(txtPrimerOperador);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "FrmCalculadora";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Calculadora de Adrian Lopez Da Silva";
+            grpSistema.ResumeLayout(false);
+            grpSistema.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtPrimerOperador;
+        private TextBox txtSegundoOperador;
+        private ComboBox cmbOperacion;
+        private Button btnOperar;
+        private Button btnLimpiar;
+        private Button btnCerrar;
+        private Label lblResultado;
+        private Label lblPrimerOperador;
+        private Label lblOperacion;
+        private Label lblSegundoOperador;
+        private GroupBox grpSistema;
+        private RadioButton rdbBinario;
+        private RadioButton rdbDecimal;
     }
 }
